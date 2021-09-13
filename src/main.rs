2b5b43fbf46add7a49f2fceafeb804618e6047c1
Bryce Clark <bryce@lowquality.dev>
2021-09-12 22:28:18 -0500

    Add basic command handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// Created by Bryce Clark (bryce@lowquality.dev)
+// Licensed under the MIT license
+// (Copy my code if you dare)
+// 
+// Spaghetti code is the only true path forward
+
 use std::{env, error::Error};
 
 use futures::stream::StreamExt;
@@ -5,8 +11,7 @@ use futures::stream::StreamExt;
 use twilight_cache_inmemory::{InMemoryCache, ResourceType};
 use twilight_gateway::{Event, Cluster, cluster::{ShardScheme}};
 use twilight_http::Client as HttpClient;
-use twilight_model::gateway::Intents;
-
+use twilight_model::gateway::{Intents, payload::MessageCreate};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -50,17 +55,16 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     Ok(())
 }
 
+static CMD_PREFIX: &'static str = "&";
+
 async fn handle_event(
     shard_id: u64,
     event: Event,
     http: HttpClient,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
     match event {
-        Event::MessageCreate(msg) if msg.content == "!ping" => {
-            http.create_message(msg.channel_id)
-                .content("Pong!~")?
-                .exec()
-                .await?;
+        Event::MessageCreate(msg) => {
+            if msg.content.starts_with(CMD_PREFIX) { handle_command(shard_id, msg, http).await? }
         }
         Event::ShardConnected(_) => {
             println!("Connected on shard {}", shard_id);
@@ -70,4 +74,61 @@ async fn handle_event(
     }
 
     Ok(())
+}
+
+async fn handle_command(shard_id: u64, msg: Box<MessageCreate>, http: HttpClient) -> Result<(), Box<dyn Error + Send + Sync>> {
+
+    let parsed = parse_command(&msg.content);
+
+    match parsed.root.as_str() {
+        "ping" => {
+            http.create_message(msg.channel_id)
+            .content("Pong!~")?
+            .exec()
+            .await?;
+        }
+        &_ => {}
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq)]
+struct UserCommand {
+    root: String,
+    arguments: Vec<String>
+}
+
+fn parse_command(content: &String) -> UserCommand {
+    // Grab the tokens without the prefix
+    let split: Vec<String> = 
+        content[CMD_PREFIX.len()..]
+            .split(' ')
+            .map(String::from)
+            .collect();
+
+    UserCommand {
+        root: split[0].clone(),
+        arguments: split[1..].to_vec()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{UserCommand, parse_command};
+
+    #[test]
+    fn test_command_parser() {
+        let test_string = String::from("&test 1 2 what the fuck");
+
+        let expected = UserCommand {
+            root: String::from("test"),
+            arguments: vec!["1", "2", "what", "the", "fuck"]
+                            .iter()
+                            .map(|s| s.to_owned().to_owned())
+                            .collect()
+        };
+
+        assert_eq!(parse_command(&test_string), expected);
+    }
 }
